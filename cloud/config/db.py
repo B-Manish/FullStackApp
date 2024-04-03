82d8351d9eb21ef62844077e4742eb0d5bf6eaf7
B-Manish <batchumanish@gmail.com>
2024-04-03 23:19:54 +0530

    added model to db.py

--- cloud/config/db.py
+++ cloud/config/db.py
@@ -1,9 +1,9 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from models.user import testuser
+from models.user import testuser,restaurants
 
 async def init():
     client=AsyncIOMotorClient("mongodb+srv://manishbatchu:Q7h5KtERcGZdfJ4E@manishcluster.71tedsw.mongodb.net/")
     db=client.manishclouddb
 
-    await init_beanie(database=db , document_models=[testuser])
+    await init_beanie(database=db , document_models=[testuser,restaurants])
