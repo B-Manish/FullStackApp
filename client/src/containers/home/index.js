42c4ffd8bf4476211374cf5f661795bffb3b4316
B-Manish <batchumanish@gmail.com>
2024-11-08 10:19:47 +0530

    Commented out lambda api

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -2,20 +2,15 @@ import { Box, Grid } from "@mui/material";
 import React, { useEffect, useState, useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import { fetchallrestaurantsdata } from "../../redux/restaurantSlice";
 import { getAllRestaurants, getLambdaapi } from "../../api/restaurantApi";
 import RestaurantCard from "../../components/RestaurantCard";
 
 function Home() {
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   dispatch(fetchallrestaurantsdata());
-  // }, []);
   const dispatch = useDispatch();
   const restData = useSelector((state) => state);
   const [data, setData] = useState([]);
-  const [ldata, setLdata] = useState("");
-  // console.log("restData", restData);
+  // const [ldata, setLdata] = useState("");
 
   const goTo = (restaurantId) => {
     navigate(`/restaurant/${restaurantId}`);
@@ -30,17 +25,17 @@ function Home() {
         console.log("err", err);
       });
 
-    getLambdaapi()
-      .then((res) => {
-        setLdata(res?.body);
-      })
-      .catch((err) => {
-        console.log("err", err);
-      });
+    // getLambdaapi()
+    //   .then((res) => {
+    //     setLdata(res?.body);
+    //   })
+    //   .catch((err) => {
+    //     console.log("err", err);
+    //   });
   }, []);
   return (
     <Box>
-      ldata:{ldata}
+      {/* ldata:{ldata} */}
       <Grid container sx={{ border: "1px solid blue", width: "80vw" }}>
         {data?.map((item, index) => (
           <Grid
