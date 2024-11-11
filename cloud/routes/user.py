3ec3aad2b146da380bcd0f588f01df29a7a48743
batchumanish <batchumanish@gmail.com>
2024-11-11 17:27:55 +0530

    Added comment

--- cloud/routes/user.py
+++ cloud/routes/user.py
@@ -291,7 +291,7 @@ async def update_item(username:str,cartitem:updatecart):
         },
         ExpressionAttributeValues={
             ':items_count': cartitem.items_count,
-            ':items': list(filter(lambda item: item['count'] != 0, cartitem.items)),  
+            ':items': list(filter(lambda item: item['count'] != 0, cartitem.items)),  #removes the items with count=0
             ':billdetails': cartitem.billdetails,
             ':branch': cartitem.branch,
             ':restaurant_id': cartitem.restaurant_id,
